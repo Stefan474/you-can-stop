3cef9b5d31179f4c3eed30817fdadf4ea8c191eb
Stefan474 <Fanste474@gmail.com>
2025-06-15 22:06:07 +0200

    add basic layout

--- src/app/games/you-can-stop/page.tsx
+++ src/app/games/you-can-stop/page.tsx
@@ -1,3 +1,9 @@
+import GameView from "@/app/games/you-can-stop/views/game-view/page";
+
 export default function YouCanStop() {
-  return <div className="bg-white w-[420px] h-[747px]"></div>;
+  return (
+    <div className="bg-white w-[420px] h-[747px]">
+      <GameView />
+    </div>
+  );
 }
--- src/app/games/you-can-stop/views/game-view/page.tsx
+++ src/app/games/you-can-stop/views/game-view/page.tsx
@@ -1,3 +1,22 @@
-export default function gameView() {
-  return <div className="bg-black">Hello</div>;
+export default function GameView() {
+  return (
+    <div className="bg-black w-full h-full text-white flex flex-col">
+      <div className="flex justify-center py-2">Top Bar </div>
+      {/* Game content goes here */}
+      <div className="flex-grow flex flex-col w-full items-center h-full">
+        {/* Game content area */}
+        <div className="flex-grow">Game Content</div>
+        {/* skill bar */}
+        <div className="grid grid-cols-3 grid-rows-2 p-4 w-full h-1/3 gap-2 ">
+          <div className="col-span-1 bg-red-500 rounded-lg">skill 1</div>
+          <div className="col-span-1 bg-red-500 rounded-lg">skill 2</div>
+
+          <div className="row-start-2 bg-red-500 col-span-1 rounded-lg">3</div>
+          <div className="row-start-2 bg-red-500 col-span-1 rounded-lg">4</div>
+
+          <div className="row-start-1 row-span-2 bg-red-500 rounded-lg">5</div>
+        </div>
+      </div>
+    </div>
+  );
 }
